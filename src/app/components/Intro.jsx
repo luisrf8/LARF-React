7fc8aede16f5c75ad88b8f16052564b480d7d45a
Luis Rojas <lrojas@tecfin212.com>
2023-07-05 15:05:31 -0400

    font

--- src/app/components/Intro.jsx
+++ src/app/components/Intro.jsx
@@ -1,22 +1,53 @@
 "use client";
-import React from 'react';
-import { Grid, Typography, Button } from '@mui/material';
+import React from "react";
+import { useEffect } from "react";
+import { Grid, Typography, Button } from "@mui/material";
+import WebFont from "webfontloader";
 
 export default function Intro() {
+  useEffect(() => {
+    WebFont.load({
+      google: {
+        families: ["Unica One", "Chilanka"],
+      },
+    });
+  }, []);
   return (
     <Grid container className="pagination" justifyContent="center">
-      <Grid item className="content items-center" sx={{display: 'flex', flexDirection: 'column'}}>
-        <Typography variant="h2" className="mb-4" data-aos="fade-up">
-        Luis Rojas, Front-End developer
+      <Grid
+        item
+        className="content items-center"
+        sx={{ display: "flex", flexDirection: "column" }}
+      >
+        <Typography
+          variant="h2"
+          className="mb-4"
+          data-aos="fade-up"
+          sx={{ fontFamily: "Unica One" }}
+        >
+          Luis Rojas, Front-End developer
         </Typography>
-        <Grid container justifyContent="center" sx={{ marginTop: '50px'}}>
-          <Grid item data-aos="fade-up" data-aos-delay="400" sx={{ marginRight: '10px'}}>
-            <Button variant="contained" className="text-white bg-gray-700 hover:bg-gray-800 w-full mb-4 sm:w-auto sm:mb-0">
+        <Grid container justifyContent="center" sx={{ marginTop: "50px" }}>
+          <Grid
+            item
+            data-aos="fade-up"
+            data-aos-delay="400"
+            sx={{ marginRight: "10px" }}
+          >
+            <Button
+              variant="contained"
+              className="text-white bg-gray-700 hover:bg-gray-800 w-full mb-4 sm:w-auto sm:mb-0"
+              sx={{ fontFamily: "Unica One" }}
+            >
               Start free trial
             </Button>
           </Grid>
           <Grid item data-aos="fade-up" data-aos-delay="600">
-            <Button variant="contained" className="text-white bg-gray-700 hover:bg-gray-800 w-full sm:w-auto sm:ml-4">
+            <Button
+              variant="contained"
+              className="text-white bg-gray-700 hover:bg-gray-800 w-full sm:w-auto sm:ml-4"
+              sx={{ fontFamily: "Unica One" }}
+            >
               Learn more
             </Button>
           </Grid>
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,15 +1,24 @@
 "use client";
 import * as React from "react";
+import { useEffect } from 'react';
 import Intro from "./components/Intro";
 import Info from "./components/Info";
 import Qualities from "./components/Qualities";
 import Projects from "./components/Projects";
 import SocialMedia from "./components/SocialMedia";
 import Section from "./utils/section";
+import WebFont from 'webfontloader';
 
 export default function Home() {
+  useEffect(() => {
+    WebFont.load({
+      google: {
+        families: ['Unica One', 'Chilanka']
+      }
+    });
+   }, []);
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between">
+    <main className="flex min-h-screen flex-col items-center justify-between" style={{fontFamily: 'Unica One'}}>
       <Section>
         <Intro />
       </Section>

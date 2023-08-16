ad7f3706782f9a64ffad6413041b4dc17af261b6
Luis Rojas <lrojas@tecfin212.com>
2023-08-16 17:39:58 -0400

    build

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -8,21 +8,14 @@ import Projects from "./components/Projects";
 import SocialMedia from "./components/SocialMedia";
 import Qualities from "./components/Qualities";
 import Section from "./utils/section";
-import WebFont from "webfontloader";
 import CustomThemeProvider from "./theme";
 
 export default function Home() {
   useEffect(() => {
-    WebFont.load({
-      google: {
-        families: ["Unica One", "Chilanka"],
-      },
-    });
   }, []);
+
   return (
-    <main
-      className="flex min-h-screen flex-col items-center justify-between"
-    >
+    <main className="flex min-h-screen flex-col items-center justify-between">
       <CustomThemeProvider>
         <Section>
           <Intro />

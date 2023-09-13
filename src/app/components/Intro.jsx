e81dd81b17443ece79c1fbd51f6503bbb3791c43
Luis Rojas <lrojas@tecfin212.com>
2023-09-13 17:56:14 -0400

    act

--- src/app/components/Intro.jsx
+++ src/app/components/Intro.jsx
@@ -3,18 +3,16 @@ import React, { useEffect, useRef } from "react";
 import { Grid, Typography, Button, Box } from "@mui/material";
 import Image from "next/image";
 import Typed from "typed.js";
-import { KeyboardArrowDown } from '@mui/icons-material';
+import { KeyboardArrowDown, PlayArrow } from '@mui/icons-material';
 
 
 export default function Intro() {
-  const typedTextRef = useRef(null);
-
   useEffect(() => {
     const options = {
       strings: [
         "Desarrollador web",
-        "Desarrollador front-end",
         "Desarrollador de aplicaciones",
+        "Desarrollador front-end",
       ],
       typeSpeed: 100,
       backSpeed: 20,
@@ -38,7 +36,7 @@ export default function Intro() {
       <Grid xs={6} className="flex justify-end">
         <Image src="/images/lr-logo-white.png" width={900} height={900}/>
       </Grid>
-      <Grid xs={6}>
+      <Grid xs={6} className="">
         <Typography
           className="display-3"
           variant="body1"
@@ -47,22 +45,23 @@ export default function Intro() {
           Luis Rojas
         </Typography>
         <Typography
-          variant="body1"
-          sx={{ fontSize: "4rem", }}
+          sx={{ fontSize: "3rem", }}
           id="typed-text-element"
         >
         </Typography>
-        <Box className="flex row mt-4">
+        <Box className="flex row mt-4 items-center">
           <Button
+            className="mr-12"
             variant="outlined"
-            sx={{color: "white", border: "1px solid white"}}
+            sx={{color: "white", border: "1px solid white", height:"3rem",}}
           >
             Download CV
           </Button>
           <Button
             sx={{color: "white", }}
             >
-            <Box className="mr-2" sx={{width: "2.5rem", height:"2.5rem", backgroundColor:"white", borderRadius:"50%"}}>
+            <Box className="mr-2" sx={{width: "5rem", height:"5rem", backgroundColor:"white", borderRadius:"50%"}}>
+              <PlayArrow sx={{ color: 'black' }}/>
             </Box>
             Ver Video
           </Button>

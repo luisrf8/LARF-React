b42c4c07d64f9d0fa9e53f9e468329a5ce1647ea
Luis Rojas <lrojas@tecfin212.com>
2023-09-13 16:08:40 -0400

    animations

--- src/app/components/Intro.jsx
+++ src/app/components/Intro.jsx
@@ -1,71 +1,76 @@
-import React from "react";
+"use client";
+import React, { useEffect, useRef } from "react";
 import { Grid, Typography, Button, Box } from "@mui/material";
 import Image from "next/image";
+import Typed from "typed.js";
+import { KeyboardArrowDown } from '@mui/icons-material';
+
+
 export default function Intro() {
+  const typedTextRef = useRef(null);
+
+  useEffect(() => {
+    const options = {
+      strings: [
+        "Desarrollador web",
+        "Desarrollador front-end",
+        "Desarrollador de aplicaciones",
+      ],
+      typeSpeed: 100,
+      backSpeed: 20,
+      smartBackspace: true,
+      loop: true,
+    };
+
+    const typed = new Typed("#typed-text-element", options);
+
+    return () => {
+      typed.destroy();
+    };
+  }, []);
+
   return (
     <Grid
       container
       className="pagination flex justify-center items-center"
       sx={{ backgroundColor: "#404040" }}
     >
-      <Grid className="flex">
-        <Grid className="flex items-center justify-center" xs={6}>
-          <Box>
-            <Image
-              src="/images/lr-logo-white.png"
-              alt=""
-              width={800}
-              height={800}
-            />
-          </Box>
-        </Grid>
-        <Grid className="flex items-center" xs={6}>
-          <Box>
-            <Typography
-              variant="h1"
-              className="display-3 text-uppercase text-primary"
-              data-aos="fade-up"
-              sx={{
-                WebkitTextStroke: "2px #ffffff",
-              }}
-            >
-              Luis Rojas
-            </Typography>
-            <Typography variant="h1" data-aos="fade-up">
-              Front-End Dev
-            </Typography>
-            <Grid container justifyContent="center" sx={{ marginTop: "50px" }}>
-              <Grid>
-                <Button
-                  className="text-white"
-                  sx={{
-                    width: "15rem",
-                    height: "3rem",
-                    fontSize: "1.5rem",
-                    border: "1px solid white",
-                  }}
-                >
-                  Download CV
-                </Button>
-              </Grid>
-              {/* <Grid item data-aos="fade-up" data-aos-delay="600">
-                <Button
-                  variant="contained"
-                  className="text-white bg-gray-800 w-full sm:w-auto sm:ml-4"
-                  sx={{
-                    width: "15rem",
-                    height: "3rem",
-                    fontSize: "1.5rem",
-                    backgroundColor: "#404040",
-                  }}
-                >
-                  Get in Touch
-                </Button>
-              </Grid> */}
-            </Grid>
-          </Box>
-        </Grid>
+      <Grid xs={6} className="flex justify-end">
+        <Image src="/images/lr-logo-white.png" width={900} height={900}/>
       </Grid>
+      <Grid xs={6}>
+        <Typography
+          className="display-3"
+          variant="body1"
+          sx={{ fontSize: "5rem", WebkitTextStroke: "2px solid #fffff"}}
+        >
+          Luis Rojas
+        </Typography>
+        <Typography
+          variant="body1"
+          sx={{ fontSize: "4rem", }}
+          id="typed-text-element"
+        >
+        </Typography>
+        <Box className="flex row mt-4">
+          <Button
+            variant="outlined"
+            sx={{color: "white", border: "1px solid white"}}
+          >
+            Download CV
+          </Button>
+          <Button
+            sx={{color: "white", }}
+            >
+            <Box className="mr-2" sx={{width: "2.5rem", height:"2.5rem", backgroundColor:"white", borderRadius:"50%"}}>
+            </Box>
+            Ver Video
+          </Button>
+        </Box>
+      </Grid>
+      <div className="scroll-arrow">
+        <KeyboardArrowDown sx={{ fontSize: '4rem' }} />
+      </div>
     </Grid>
   );
 }
